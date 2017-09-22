c5836d5ca106d30e71b58a71341a66140c776550
Elio Rosabal <elio_erp@hotmail.com>
2017-09-21 20:15:39 -0400

    Book table and first migrations

---
+++ BookMaster/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookMaster.Models
+{
+    public class Book
+    {
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public double Price { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+        
+        public string Publisher { get; set; }
+
+        [MaxLength(17)]
+        [Key]
+        public string ISBN { get; set; }
+
+    }
+}
