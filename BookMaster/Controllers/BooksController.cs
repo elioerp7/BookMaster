35f370faa36c5efbcc3fa82461bf3765e0074de7
Elio Rosabal <elio_erp@hotmail.com>
2017-09-29 18:42:24 -0400

    shopping cart redirect

---
+++ BookMaster/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BookMaster.Controllers
+{
+    public class BooksController : Controller
+    {
+        // GET: Books
+        public ActionResult ShoppingCart()
+        {
+            return View();
+        }
+    }
+}
